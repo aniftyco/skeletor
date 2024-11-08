5166329609959d9076e36e6f9171db747c850df1
Josh Manders <josh@joshmanders.com>
2024-11-07 23:10:55 -0600

    add basic cleanup

--- src/Runner.php
+++ src/Runner.php
@@ -6,19 +6,33 @@ use Composer\Script\Event;
 
 class Runner
 {
+    static $cwd = null;
+
     public static function execute(Event $event): void
     {
+        static::$cwd = getcwd();
+
         require_once __DIR__ . '/../../../autoload.php';
 
-        $cwd = getcwd();
-
-        $skeletor = new Skeletor($cwd, $event);
+        $skeletor = new Skeletor(static::$cwd, $event);
 
         try {
-            $handler = require_once $cwd . '/Skeletorfile.php';
+            $handler = require_once static::$cwd . '/Skeletorfile.php';
             $handler($skeletor);
+            static::cleanup();
         } catch (\Throwable $e) {
             $event->getIO()->error($e->getMessage());
         }
+    }
+
+    private static function cleanup(): void
+    {
+        $composerJson = json_decode(file_get_contents(static::$cwd . '/composer.json'), true);
+
+        unlink(static::$cwd . '/Skeletorfile.php');
+
+        unset($composerJson['scripts']['post-create-project-cmd']);
+
+        file_put_contents(static::$cwd . '/composer.json', json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
     }
 }
