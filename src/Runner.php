4d9b839c526adeaea84a01c56895117734a22ee5
Josh Manders <josh@joshmanders.com>
2024-11-15 16:01:11 -0600

    format with pint

--- src/Runner.php
+++ src/Runner.php
@@ -6,20 +6,21 @@ use Composer\Script\Event;
 
 class Runner
 {
-    static ?Event $event = null;
-    static ?string $cwd = null;
+    public static ?Event $event = null;
+
+    public static ?string $cwd = null;
 
     public static function execute(Event $event): void
     {
         static::$event = $event;
         static::$cwd = getcwd();
 
-        require_once __DIR__ . '/../../../autoload.php';
+        require_once __DIR__.'/../../../autoload.php';
 
         $skeletor = new Skeletor(static::$cwd, $event);
 
         try {
-            $handler = require_once static::$cwd . '/Skeletorfile.php';
+            $handler = require_once static::$cwd.'/Skeletorfile.php';
             $handler($skeletor);
             if ((bool) getenv('NO_SKELETOR_CLEANUP') === false) {
                 static::cleanup();
@@ -31,13 +32,13 @@ class Runner
 
     private static function cleanup(): void
     {
-        $composerJson = json_decode(file_get_contents(static::$cwd . '/composer.json'), true);
+        $composerJson = json_decode(file_get_contents(static::$cwd.'/composer.json'), true);
 
-        @unlink(static::$cwd . '/Skeletorfile.php');
+        @unlink(static::$cwd.'/Skeletorfile.php');
 
         unset($composerJson['scripts']['post-create-project-cmd']);
 
-        @file_put_contents(static::$cwd . '/composer.json', json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        @file_put_contents(static::$cwd.'/composer.json', json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 
         @exec('composer remove --dev aniftyco/skeletor');
     }
